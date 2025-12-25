60b16e0fe87148a72fdbe8414905c52b7361c1c6
Iago Guedes <f4k3fun@gmail.com>
2025-12-25 14:46:50 -0300

    Renderizando os horarios

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,6 +2,8 @@ import dayjs from "dayjs";
 
 import { openingHours } from "../../utils/opening-hours";
 
+const hours = document.getElementById("hours");
+
 export function hoursLoad({date}){
   const opening = openingHours.map((hoursOpening) => {
     const [hours, _] = hoursOpening.split(":");
@@ -10,10 +12,38 @@ export function hoursLoad({date}){
 
     return {
       hour: hours,
-      isValidat: isValidate,
+      avaliable: isValidate,
     }
   })
 
 
-  console.log(opening);
+  opening.forEach(({hour, avaliable}) => {
+    const li = document.createElement("li");
+    li.classList.add("hour");
+    li.classList.add(avaliable ? "hour-available" : "hour-unavailable");
+
+    li.textContent = hour
+
+    hours.append(li);
+
+
+    if(hour === "09"){
+      hourHeaderAdd({title: "Manha"})
+    } else if(hour === "12") {
+      hourHeaderAdd({title: "Tarde"})
+    } else if(hour === "17"){
+      hourHeaderAdd({title: "Noite"})
+    }
+
+  })
+}
+
+function hourHeaderAdd({title}){
+  const header = document.createElement("li");
+  header.classList.add("hour-period");
+
+  header.textContent = title;
+
+
+  hours.append(header)
 }
